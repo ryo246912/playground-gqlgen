c4cd1d8f02bb1bef114d21ef8d35278ef2d84e48
ryo246912 <r-k.sky_25cloudf@hotmail.co.jp>
2025-05-27 21:30:23 +0900

    chore: userのresolveを別に分ける

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -16,11 +16,19 @@ import (
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
 	randNumber, _ := rand.Int(rand.Reader, big.NewInt(100))
+
+	// todo := &model.Todo{
+	// 	Text: input.Text,
+	// 	ID:   fmt.Sprintf("T%d", randNumber),
+	// 	User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	// }
+	// NOTE:userIDをresolveする
 	todo := &model.Todo{
-		Text: input.Text,
-		ID:   fmt.Sprintf("T%d", randNumber),
-		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+		Text:   input.Text,
+		ID:     fmt.Sprintf("T%d", randNumber),
+		UserID: input.UserID,
 	}
+
 	r.todos = append(r.todos, todo)
 	return todo, nil
 }
@@ -30,11 +38,21 @@ func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
 	return r.todos, nil
 }
 
+// User is the resolver for the user field.
+// Note:resolver:trueにしたことで新たに生成された関数
+func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, error) {
+	return &model.User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
+}
+
 // Mutation returns MutationResolver implementation.
 func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 
 // Query returns QueryResolver implementation.
 func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
+// Todo returns TodoResolver implementation.
+func (r *Resolver) Todo() TodoResolver { return &todoResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type todoResolver struct{ *Resolver }
